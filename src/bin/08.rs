08c9c00e46f9a684e28a3c87d74f483429932ee1
Ankomat <ankomat@live.be>
2021-12-28 21:09:01 +0100

    "nearly" finished day 8 part 2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -8,7 +8,7 @@ use std::time::Instant;
 // --------------------------------------------
 fn main() {
     day8_part1();
-    // day8_part2();
+    day8_part2();
 }
 // --------------------------------------------
 fn day8_part1() {
@@ -27,203 +27,101 @@ fn day8_part1() {
     println!("There are {} outputs of length 2, 3, 4 or 7", count);
 }
 
-// fn day7_part2v1() {
-//     // read from file into Vector of strings
-// 	let input = read_file("input/07.txt");
-// 	let v: Vec<_> = input.trim().split(',').map(|x| x.parse::<i64>().unwrap()).collect();
-//     let mut iter = v.iter();
-//     let mut horizontal_positions = HashMap::new();
-//     let lower: i64 = *v.iter().min().unwrap();
-//     let upper: i64 = *v.iter().max().unwrap()+1;
-//     // make vector-map of crab positions
-//     loop {
-//         match iter.next() {
-//             Some(position) => {
-//                 let count = horizontal_positions.entry(*position).or_insert(0);
-//                 *count += 1;
-//             }
-//             None => break
-//         }
-//     }
-//     // recursion in for loop
-//     let now = Instant::now();
-//     let mut minimum_fuel = 0;
-//     for key in lower..upper {
-//         let mut fuel: i64 = 0;
-//         for (pos, num) in &horizontal_positions {
-//             fuel += fuel_rec((*pos as i64 - key as i64).abs()) * num;
-//         }
-//         if key == lower || fuel < minimum_fuel {
-//             minimum_fuel = fuel;
-//         }
-//     }
-//     println!("recursion in for loop: {:?}", now.elapsed().as_secs_f64());
-//     println!("minimum fuel needed is {:?}", minimum_fuel);
-// }
-//
-// fn day7_part2v2() {
-//     // read from file into Vector of strings
-// 	let input = read_file("input/07.txt");
-// 	let v: Vec<_> = input.trim().split(',').map(|x| x.parse::<i64>().unwrap()).collect();
-//     let mut iter = v.iter();
-//     let mut horizontal_positions = HashMap::new();
-//     let lower: i64 = *v.iter().min().unwrap();
-//     let upper: i64 = *v.iter().max().unwrap()+1;
-//     // make vector-map of crab positions
-//     loop {
-//         match iter.next() {
-//             Some(position) => {
-//                 let count = horizontal_positions.entry(*position).or_insert(0);
-//                 *count += 1;
-//             }
-//             None => break
-//         }
-//     }
-//     // recursion in iterator closure
-//     let now = Instant::now();
-//     let mut minimum_fuel = 0;
-//     for key in lower..upper {
-//         let fuel: i64 = horizontal_positions.iter().map(|(pos, num)| fuel_rec((*pos as i64 - key as i64).abs()) * *num as i64).sum();
-//         if key == lower || fuel < minimum_fuel {
-//             minimum_fuel = fuel;
-//         }
-//     }
-//     println!("recursion in iterator closure: {:?}", now.elapsed().as_secs_f64());
-//     println!("minimum fuel needed is {:?}", minimum_fuel);
-// }
-//
-// fn day7_part2v3() {
-//     // read from file into Vector of strings
-// 	let input = read_file("input/07.txt");
-// 	let v: Vec<_> = input.trim().split(',').map(|x| x.parse::<i64>().unwrap()).collect();
-//     let mut iter = v.iter();
-//     let mut horizontal_positions = HashMap::new();
-//     let lower: i64 = *v.iter().min().unwrap();
-//     let upper: i64 = *v.iter().max().unwrap()+1;
-//     // make vector-map of crab positions
-//     loop {
-//         match iter.next() {
-//             Some(position) => {
-//                 let count = horizontal_positions.entry(*position).or_insert(0);
-//                 *count += 1;
-//             }
-//             None => break
-//         }
-//     }
-//     // iteration in iterator closure
-//     let now = Instant::now();
-//     let mut minimum_fuel = 0;
-//     for key in lower..upper {
-//         let fuel: i64 = horizontal_positions.iter().map(|(pos, num)| fuel_ite((*pos as i64 - key as i64).abs()) * *num as i64).sum();
-//         if key == lower || fuel < minimum_fuel {
-//             minimum_fuel = fuel;
-//         }
-//     }
-//     println!("iteration in iterator closure: {:?}", now.elapsed().as_secs_f64());
-//     println!("minimum fuel needed is {:?}", minimum_fuel);
-// }
-//
-// fn day7_part2v4() {
-//     // read from file into Vector of strings
-// 	let input = read_file("input/07.txt");
-// 	let v: Vec<_> = input.trim().split(',').map(|x| x.parse::<i64>().unwrap()).collect();
-//     let mut iter = v.iter();
-//     let mut horizontal_positions = HashMap::new();
-//     let lower: i64 = *v.iter().min().unwrap();
-//     let upper: i64 = *v.iter().max().unwrap()+1;
-//     // make vector-map of crab positions
-//     loop {
-//         match iter.next() {
-//             Some(position) => {
-//                 let count = horizontal_positions.entry(*position).or_insert(0);
-//                 *count += 1;
-//             }
-//             None => break
-//         }
-//     }
-//     // iteration in for loop
-//     let now = Instant::now();
-//     let mut minimum_fuel = 0;
-//     for key in lower..upper {
-//         let mut fuel: i64 = 0;
-//         for (pos, num) in &horizontal_positions {
-//             fuel += fuel_ite((*pos as i64 - key as i64).abs()) * num;
-//         }
-//         if key == lower || fuel < minimum_fuel {
-//             minimum_fuel = fuel;
-//         }
-//     }
-//     println!("iteration in for loop: {:?}", now.elapsed().as_secs_f64());
-//     println!("minimum fuel needed is {:?}", minimum_fuel);
-// }
-//
-// fn day7_part2v5() {
-//     // read from file into Vector of strings
-// 	let input = read_file("input/07.txt");
-// 	let v: Vec<_> = input.trim().split(',').map(|x| x.parse::<i64>().unwrap()).collect();
-//     let mut iter = v.iter();
-//     let mut horizontal_positions = HashMap::new();
-//     let lower: i64 = *v.iter().min().unwrap();
-//     let upper: i64 = *v.iter().max().unwrap()+1;
-//     // make vector-map of crab positions
-//     loop {
-//         match iter.next() {
-//             Some(position) => {
-//                 let count = horizontal_positions.entry(*position).or_insert(0);
-//                 *count += 1;
-//             }
-//             None => break
-//         }
-//     }
-//     // cached iteration in for loop
-//     let now = Instant::now();
-//     let mut minimum_fuel = 0;
-//     let mut fuel_cache: HashMap<i64, i64> = HashMap::new();
-//     // let mut cache_hit = 0_u64;
-//     // let mut loops = 0_u64;
-//     for key in lower..upper {
-//         let mut fuel: i64 = 0;
-//         for (pos, num) in &horizontal_positions {
-//             // loops += 1;
-//             let delta = (*pos as i64 - key as i64).abs();
-//             let temp = fuel_cache.entry(delta).or_insert(fuel_ite(delta));
-//             fuel += *temp * num;
-//             // fuel += match fuel_cache.get(&delta) {
-//             //     Some(f) => {
-//             //         cache_hit += 1;
-//             //         *f * num
-//             //     },
-//             //     None => {
-//             //         let temp = fuel_ite(delta);
-//             //         fuel_cache.insert(delta, temp);
-//             //         temp * num
-//             //     },
-//             // }
-//         }
-//         if key == lower || fuel < minimum_fuel {
-//             minimum_fuel = fuel;
-//         }
-//     }
-//     println!("cached iteration in for loop: {:?}", now.elapsed().as_secs_f64());
-//     println!("minimum fuel needed is {:?}", minimum_fuel);
-//     // println!("cache hit {:?} times in {:?} loops.", cache_hit, loops);
-// }
-//
-// fn fuel_rec(i: i64) -> i64 {
-//     if i == 0 {
-//         0
-//     } else {
-//         i + fuel_rec(i-1)
-//     }
-// }
-//
-// fn fuel_ite(i: i64) -> i64 {
-//     let mut fuel = 0;
-//     for i in 1..i+1 {
-//         fuel += i;
-//     }
-//     fuel
-// }
+fn day8_part2() {
+    // read from file into Vector of strings
+	let input = read_file("input/08.txt");
+    let v: Vec<_> = input.trim().split('\n').collect();
+    let mut inputs = Vec::new();
+    let mut outputs = Vec::new();
+    for line in v {
+        let mut temp = line.split(" | ");
+        inputs.push(temp.next().unwrap());
+        outputs.push(temp.next().unwrap());
+    }
+    for (i, line) in inputs.iter().enumerate() {
+        let mut wirings: HashMap<_,_> = "abcdefg".chars().zip([0b111_1111;7]).collect();
+        let mut five_buffer = String::new();
+        let mut six_buffer = String::new();
+        // let mut temp = 0b111_1111;
+        for word in line.split(' ') {
+            // match word.len() {
+            //     2 => temp = 0b011_0000,
+            //     3 => temp = 0b111_0000,
+            //     4 => temp = 0b011_0011,
+            //     5 => five_buffer.push_str(word),
+            //     6 => six_buffer.push_str(word),
+            //     _ => (),
+            // }
+            // for ch in word.chars() {
+            //     wirings = wirings.into_iter().map(|(x,y)| {if x == ch {(x,y&temp)} else {(x,y)}}).collect();
+            // }
+            match word.len() {
+                2 => for ch in word.chars() {
+                    let temp = wirings.get_mut(&ch).unwrap();
+                    *temp &= 0b011_0000;
+                    // for wire in wirings.iter() {
+                    //     print!("{} = {:#09b};", wire.0, wire.1);
+                    // }
+                    // print!("\n");
+                },
+                3 => for ch in word.chars() {
+                    let temp = wirings.get_mut(&ch).unwrap();
+                    *temp &= 0b111_0000;
+                },
+                4 => for ch in word.chars() {
+                    let temp = wirings.get_mut(&ch).unwrap();
+                    *temp &= 0b011_0011;
+                },
+                5 => {
+                    five_buffer.push_str(word);
+                    // println!("5 letter word, 5-buffer is {}", five_buffer);
+                },
+                6 => {
+                    six_buffer.push_str(word);
+                },
+                _ => (),
+            }
+        }
+        // println!("finished short words in line {}", i);
+        let mut five_map = count_chars(&five_buffer);
+        for (ch, num) in five_map.iter() {
+            match num {
+                1 => {
+                    let temp = wirings.get_mut(&ch).unwrap();
+                    *temp &= 0b000_0110;
+                },
+                2 => {
+                    let temp = wirings.get_mut(&ch).unwrap();
+                    *temp &= 0b011_0000;
+                },
+                3 => {
+                    let temp = wirings.get_mut(&ch).unwrap();
+                    *temp &= 0b100_1001;
+                },
+                _ => (),
+            }
+        }
+        let mut six_map = count_chars(&six_buffer);
+        for (ch, num) in six_map.iter() {
+            match num {
+                2 => {
+                    let temp = wirings.get_mut(&ch).unwrap();
+                    *temp &= 0b010_0101;
+                },
+                3 => {
+                    let temp = wirings.get_mut(&ch).unwrap();
+                    *temp &= 0b101_1010;
+                },
+                _ => (),
+            }
+        }
+        println!("after line {}:", i);
+        for wire in wirings.iter() {
+            print!("{} = {:#09b};", wire.0, wire.1);
+        }
+        print!("\n");
+    }
+}
 
 fn read_file<P>(filename: P) -> String where P: AsRef<Path>, {
     let mut buffer = String::new();
@@ -233,4 +131,13 @@ fn read_file<P>(filename: P) -> String where P: AsRef<Path>, {
 
 fn type_of<T>(_: T) -> &'static str {
     type_name::<T>()
+}
+
+fn count_chars(word: &str) -> HashMap<char,usize> {
+    let mut char_count = HashMap::new();
+    for ch in word.chars() {
+        let count = char_count.entry(ch).or_insert(0);
+        *count += 1;
+    }
+    char_count
 }
