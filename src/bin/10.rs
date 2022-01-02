c51be2fa0ae2805142cb265339f47fffc649d6b1
Ankomat <ankomat@live.be>
2022-01-02 23:26:45 +0100

    finished day 10 part 2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -7,28 +7,31 @@ use std::any::type_name;
 use std::time::Instant;
 // --------------------------------------------
 fn main() {
+    // read from file into Vector of strings
     let input = read_file("input/10.txt");
-    let v: Vec<_> = input.trim().split('\n').collect();
-    day10_part1(&v);
-    // day10_part2(&v, &mut low_points);
+    let mut v: Vec<_> = input.trim().split('\n').collect();
+    day10_part1(&mut v);
+    day10_part2(&mut v);
 }
 // --------------------------------------------
-// read from file into Vector of strings
 
-fn day10_part1(v: &Vec<&str>) {
+fn day10_part1(v: &mut Vec<&str>) {
     let mut stack: Vec<char> = Vec::new();
     let mut count = 0;
-    for line in v.iter() {
+    // println!("Beginning lines = {}", v.len());
+    let mut j = 0; //keep track of deleted lines because counter is not correct after v.remove()
+    for (i, line) in v.clone().iter().enumerate() {
+        // println!("Removed lines = {}",j);
         for ch in line.chars() {
             match ch {
                 '(' => stack.push(')'),
                 '{' => stack.push('}'),
                 '[' => stack.push(']'),
                 '<' => stack.push('>'),
-                ')' => if ')' != stack.pop().unwrap() { count += 3; },
-                '}' => if '}' != stack.pop().unwrap() { count += 1197; },
-                ']' => if ']' != stack.pop().unwrap() { count += 57; },
-                '>' => if '>' != stack.pop().unwrap() { count += 25137; },
+                ')' => if ')' != stack.pop().unwrap() { count += 3; v.remove(i-j); j+=1; },
+                '}' => if '}' != stack.pop().unwrap() { count += 1197; v.remove(i-j); j+=1; },
+                ']' => if ']' != stack.pop().unwrap() { count += 57; v.remove(i-j); j+=1; },
+                '>' => if '>' != stack.pop().unwrap() { count += 25137; v.remove(i-j); j+=1; },
                 _ => (),
             };
         }
@@ -36,48 +39,41 @@ fn day10_part1(v: &Vec<&str>) {
     println!("Illegal score = {}", count);
 }
 
-// fn day10_part2(v: &Vec<&str>, low_points: &HashSet<(usize,usize)>) {
-//     let mut basins = HashMap::new();
-//     for (i,j) in low_points {
-//         let mut basin_size = 0;
-//         let mut checked = HashSet::new();
-//         basin_size += count_size(*i, *j, &mut checked, &v);
-//         // println!("Low point ({},{}) has basin size {}", i, j, basin_size);
-//         basins.insert((i,j), basin_size);
-//     }
-//     println!("{:?}", &basins);
-//     println!("");
-//     let mut temp = basins.values().cloned().collect::<Vec<usize>>();
-//     temp.sort();
-//     temp.reverse();
-//     println!("{:?}", temp.get(0).unwrap()*temp.get(1).unwrap()*temp.get(2).unwrap());
-// }
-//
-// fn count_size(i: usize, j: usize, checked: &mut HashSet<(usize,usize)>, v: &Vec<&str>) -> usize {
-//     let mut height = v.get(i).unwrap().chars().nth(j).unwrap().to_digit(10).unwrap();;
-//     let mut count = 0;
-//     if !checked.contains(&(i,j)) && height < 9 {
-//         checked.insert((i,j));
-//         count += 1;
-//         if i > 0 {
-//             count += count_size(i-1, j, checked, &v);
-//         }
-//         if i < 99 {
-//             count += count_size(i+1, j, checked, &v);
-//         }
-//         if j > 0 {
-//             count += count_size(i, j-1, checked, &v);
-//         }
-//         if j < 99 {
-//             count += count_size(i, j+1, checked, &v);
-//         }
-//     }
-//     count
-// }
-//
-// fn get_el(m: &Vec<&str>, row: usize, col: usize) -> Option<char> {
-//     m.get(row)?.chars().nth(col)
-// }
+fn day10_part2(v: &mut Vec<&str>) {
+    let mut scores: Vec<u64> = Vec::new();
+    for (i,line) in v.clone().iter().enumerate() { // only incomplete lines remain
+        let mut stack: Vec<char> = Vec::new();
+        let mut line_score: u64 = 0;
+        // println!("line {} = {} needs:", i, line);
+        for ch in line.chars() {
+            match ch {
+                '(' => { stack.push(')'); None},
+                '{' => { stack.push('}'); None},
+                '[' => { stack.push(']'); None},
+                '<' => { stack.push('>'); None},
+                ')' => stack.pop(),
+                '}' => stack.pop(),
+                ']' => stack.pop(),
+                '>' => stack.pop(),
+                _ => None,
+            };
+        }
+        stack.reverse();
+        for ch in stack {
+            line_score *= 5;
+            line_score += match ch {
+                ')' => 1,
+                ']' => 2,
+                '}' => 3,
+                '>' => 4,
+                _ => 0,
+            }
+        }
+        scores.push(line_score);
+    }
+    scores.sort();
+    println!("median score is: {:?}", scores.get(scores.len()/2));
+}
 
 fn read_file<P>(filename: P) -> String where P: AsRef<Path>, {
     let mut buffer = String::new();
